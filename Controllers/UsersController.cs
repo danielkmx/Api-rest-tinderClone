7eeabcaa644f3e13f33f9bdf5ead2c9976e6f97a
DESKTOP-MB26L7H\kmx <danielbezerrakmx@gmail.com>
2018-07-01 12:05:23 -0300

    Initial commit

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
